0fb7737635acbc27a155725bfc20f9a74c0875bf
haasson <haasson22@gmail.com>
2019-04-19 20:36:30 +0700

    Сверстан блок before, добавлен функционал слайдера, оформлены эффекты кнопок и меню

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var menuButton = document.querySelector(".navbar__menu-button");
+var menuList = document.querySelector('.menu');
+menuButton.addEventListener('click', function(){
+   menuList.classList.toggle('menu__active');
+   menuButton.classList.toggle('navbar__menu-button_active')
+})
+
+var likeButtons = document.querySelectorAll('.catalog__button_like');
+for (let i = 0; i < likeButtons.length; i++) {
+   likeHandler(likeButtons[i])
+}
+   
+function likeHandler(likeButton) {
+   likeButton.addEventListener('click', function () {
+      likeButton.innerHTML = '<img src="img/catalog/heart-active.png" alt=""></img>'
+   })
+}
+
+
+// jQuery plugins
+
+$(window).on("load", function () {
+   $(".before__box").twentytwenty({
+      default_offset_pct: 0.3,
+      before_label: 'Без скинали',
+      after_label: 'Со скинали',
+   });
+   $('.before__slider').slick({
+      draggable: false,
+      dots: true,
+      dotsClass: 'before__slider-dots',
+      prevArrow: $('.arrow-left'),
+      nextArrow: $('.arrow-right')
+   })
+})
