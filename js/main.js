168271afbdaf0e76972623d39e676c92921bb916
haasson <haasson22@gmail.com>
2019-04-24 19:13:37 +0700

    Сделал адаптив и слайдер для блока reviews

--- js/main.js
+++ js/main.js
@@ -10,11 +10,8 @@ for (let i = 0; i < likeButtons.length; i++) {
    likeHandler(likeButtons[i])
 }
    
-function likeHandler(likeButton) {
-   likeButton.addEventListener('click', function () {
-      likeButton.innerHTML = '<img src="img/catalog/heart-active.png" alt=""></img>'
-   })
-}
+let reviewsSlider = document.querySelector('.reviews__list')
+window.onresize = changeReviewsType;
 
 
 // jQuery plugins
@@ -26,6 +23,7 @@ $(window).on("load", function () {
       before_label: 'Без скинали',
       after_label: 'Со скинали',
    });
+
    $('.before__slider').slick({
       draggable: false,
       swipe: false,
@@ -34,4 +32,29 @@ $(window).on("load", function () {
       prevArrow: $('.arrow-left'),
       nextArrow: $('.arrow-right')
    })
+
+   $('.reviews__slider').slick({
+      dots: true,
+      dotsClass: 'reviews__slider-dots',
+      prevArrow: $('.arrow-left'),
+      nextArrow: $('.arrow-right'),
+      
+   })
+
+
 })
+
+function likeHandler(likeButton) {
+   likeButton.addEventListener('click', function () {
+      likeButton.innerHTML = '<img src="img/catalog/heart-active.png" alt=""></img>'
+   })
+}
+
+function changeReviewsType() {
+   if (document.body.clientWidth < 768) {
+      reviewsSlider.classList.add('reviews__slider');
+   }
+   else {
+      reviewsSlider.classList.remove('reviews__slider');
+   }
+}
